c445c152e1b1e701bd4941cd0cafad720bbe679c
LonlyCat <lbs19312@live.com>
2018-03-19 15:15:49 +0800

    Initial Commit

---
+++ SwiftyNavigationBar/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  SwiftyNavigationBar
+//
+//  Created by meyoutao-3 on 2018/3/19.
+//  Copyright © 2018年 meyoutao-3. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
